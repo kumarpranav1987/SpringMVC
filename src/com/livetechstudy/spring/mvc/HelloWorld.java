1327a67c00917c03ab956d15aedb96936376035e
pranav.kumar@iongroup.com <pranav.kumar@iongroup.com>
2019-05-06 12:28:25 +0530

    Welcome Page Updated

--- src/com/livetechstudy/spring/mvc/HelloWorld.java
+++ src/com/livetechstudy/spring/mvc/HelloWorld.java
@@ -1,9 +1,21 @@
 package com.livetechstudy.spring.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloWorld {
 
+	@GetMapping("/hello")
+	public ModelAndView helloWorld() {
+ 
+		String message = "Message From Hello World";
+		return new ModelAndView("helloworld", "message", message);
+	}
 	
+	@GetMapping("/")
+	public String welcomePage() {
+		return "welcome";
+	}
 }
