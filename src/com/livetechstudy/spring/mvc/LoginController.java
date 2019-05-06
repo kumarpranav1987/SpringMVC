004c6f6722179b8eaf38c00b10f9c605bedd565a
pranav.kumar@iongroup.com <pranav.kumar@iongroup.com>
2019-05-06 18:08:24 +0530

    Service Autowire

--- src/com/livetechstudy/spring/mvc/LoginController.java
+++ src/com/livetechstudy/spring/mvc/LoginController.java
@@ -1,5 +1,6 @@
 package com.livetechstudy.spring.mvc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +9,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
-
+	
+    @Autowired
+	LoginService service;
+	
 	@GetMapping("/login")
 	public String showLoginPage() {
 		return "login";
@@ -16,7 +20,7 @@ public class LoginController {
 	
 	@PostMapping("/validate")
 	public String validate(@RequestParam String name,@RequestParam String password,ModelMap model) {
-		LoginService service = new LoginService();
+		
 		if(service.isValid(name, password)) {
 			model.put("name", name);
 			return "showdetails";
--- src/com/livetechstudy/spring/mvc/LoginService.java
+++ src/com/livetechstudy/spring/mvc/LoginService.java
@@ -1,5 +1,8 @@
 package com.livetechstudy.spring.mvc;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class LoginService {
 
 	public boolean isValid(String name, String password) {
